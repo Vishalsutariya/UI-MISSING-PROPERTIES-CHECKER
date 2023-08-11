baf7933c24771bcad8f7b63dc47f715d9453e55c
VishalS <VishalS@quinnox.com>
2023-08-11 15:37:16 +0530

    done automation to add missing properties to property files

--- compareEnvironments.js
+++ compareEnvironments.js
@@ -1,30 +1,32 @@
 import fetch from 'node-fetch';
-
+import fs from 'fs/promises';
+import path from 'path';
+import { Octokit } from '@octokit/rest';
 
 const repoOwner = 'QQyrus';
 const repoName = 'ui-configuration';
-const accessToken = 'add-your-github-generated-token';
+const accessToken = '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';
 
 // Define the base folder and the folders to compare
 const baseFolder = 'stg-ui-config';
-const foldersToCompare = ['prod-ui-config', 'qyrus-ui-config']; // Add more folders as needed
+const foldersToCompare = ['prod-ui-config']; // Add more folders as needed
 
 async function fetchEnvironmentFileContent(filePath) {
   const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
-  
   try {
     const response = await fetch(apiUrl, {
         headers: {
             Authorization: `Bearer ${accessToken}`,
         },
     });
+
     if (response.ok) {
         const data = await response.json();
         const fileContentResponse = await fetch(data.download_url);
         const fileContent = await fileContentResponse.text();
         return fileContent;
     } else {
-      throw new Error(`Failed to fetch ${filePath}/environment.prod.ts`);
+      throw new Error(`Failed to fetch ${filePath}`);
     }
   } catch (error) {
     console.error(error);
@@ -72,6 +74,12 @@ async function compareEnvironments() {
     // const missingProperties = baseProperties.filter(prop => !propertiesToCompare.includes(prop));
     missingProperties.forEach(prop => console.log(prop));
 
+    if (missingProperties.length > 0) {
+      await addMissingPropertiesToFile(folder, contentToCompare, missingProperties);
+    } else {
+      console.log(`No missing properties in ${folder}`);
+    }
+
     console.log('-----------------------');
   }
 }
@@ -85,6 +93,60 @@ function extractProperties(fileContent) {
   }
   console.log("match:",matches)
   return matches || [];
+}
+
+async function addMissingPropertiesToFile(folder, contentOnTargetFolder, missingProperties){
+  // const lines = contentOnTargetFolder.split('\n');
+
+  // // console.log("Lines", lines);
+  // // Add the missing properties to the content
+  // const updatedContent = lines.map(line => {
+  //   if (line.includes('export const environment = {')) {
+  //     return [...line, ...missingProperties].join(',\n');
+  //   }
+  //   return line;
+  // }).join('\n');
+
+  const insertionPoint = contentOnTargetFolder.lastIndexOf('}');
+    if (insertionPoint !== -1) {
+      const propertiesToAdd = missingProperties.map(prop => `  ${prop}: 'NEW_VALUE',`).join('\n');
+      contentOnTargetFolder = contentOnTargetFolder.slice(0, insertionPoint) + propertiesToAdd + contentOnTargetFolder.slice(insertionPoint);
+    }
+
+  console.log('Added missing properties: ', contentOnTargetFolder);
+
+  await commitChanges(folder, contentOnTargetFolder);
+}
+
+async function commitChanges(folder, content) {
+  const octokit = new Octokit({
+    auth: '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' // Replace with your GitHub Personal Access Token
+  });
+
+  const commitMessage = 'Added Missing Properties';
+
+  try {
+
+    // Fetch the current SHA-1 hash of the file
+    const fileInfo = await octokit.rest.repos.getContent({
+      owner: 'QQyrus', // Replace with your GitHub username
+      repo: 'ui-configuration',
+      path: `${folder}/environment.prod.ts`
+    });
+
+    const response = await octokit.rest.repos.createOrUpdateFileContents({
+      owner: 'QQyrus', // Replace with your GitHub username
+      repo: 'ui-configuration',
+      path: `${folder}/environment.prod.ts`,
+      message: commitMessage,
+      content: Buffer.from(content).toString('base64'),
+      sha: fileInfo.data.sha // Provide the current SHA-1 hash of the file
+    });
+
+    console.log(`Changes committed to ${folder}/environment.prod.ts: ${response.data.commit.html_url}`);
+  } catch (error) {
+    console.error(`Failed to commit changes to ${folder}/environment.prod.ts`, error);
+  }
 }
 
 compareEnvironments();
