de5c2bcadfb66a4c9e512cd3a7f434a0bd504a11
VishalS <VishalS@quinnox.com>
2023-09-04 10:56:58 +0530

    API Updated

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import fetch from 'node-fetch';
-import fs from 'fs/promises';
-import path from 'path';
 import { Octokit } from '@octokit/rest';
 
 const app = express();
@@ -11,167 +9,173 @@ app.use(express.json());
 
 const repoOwner = 'QQyrus';
 const repoName = 'ui-configuration';
-const accessToken = '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';
+const accessToken = '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';
 
 // Define the base folder and the folders to compare
 const baseFolder = 'stg-ui-config';
-const foldersToCompare = ['prod-ui-config']; // Add more folders as needed
+const foldersToCompare = ['adib-ui-config', 'bcg-ui-config', 'ccep-ui-config', 'cleco-ui-config', 'johndeere-ui-config', 'littelfuse-ui-config',
+  'mashreq-ui-config', 'maxdigital-ui-config', 'monument-ui-config', 'prod-ui-config', 'qyrus-ui-config', 'qyrus-uk-ui-config',
+  'rccd-ui-config', 'shawbrook-ui-config', 'sia-ui-config', 'truist-ui-config', 'tsb-ui-config', 'uat-ui-config', 'unum-ui-config',
+  'valley-ui-config', 'vitas-ui-config', 'wm-ui-config']; // Add more folders as needed
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the API for comparing and adding missing properties.');
+  res.send('Welcome to the API for comparing and adding missing properties.');
 });
 
-app.post('/compare-and-add', async (req, res) => {
-    const { folder } = req.body;
+app.get('/compare-and-add', async (res) => {
 
-    const baseContent = await fetchEnvironmentFileContent(`${baseFolder}/environment.prod.ts`);
-    if (!baseContent) {
-        console.error(`Failed to fetch ${baseFolder}/environment.prod.ts`);
-        return;
+  const baseContent = await fetchEnvironmentFileContent(`${baseFolder}/environment.prod.ts`);
+  if (!baseContent) {
+    console.error(`Failed to fetch ${baseFolder}/environment.prod.ts`);
+    return;
+  }
+
+  console.log("extracting base properties...")
+  const baseProperties = extractProperties(baseContent);
+  console.log("base properties extracted...")
+
+  for (const folder of foldersToCompare) {
+    const contentToCompare = await fetchEnvironmentFileContent(`${folder}/environment.prod.ts`);
+
+    if (!contentToCompare) {
+      console.error(`Failed to fetch ${folder}/environment.prod.ts`);
+      continue;
     }
 
-    for (const folder of foldersToCompare) {
-        const contentToCompare = await fetchEnvironmentFileContent(`${folder}/environment.prod.ts`);
+    console.log("extracting target properties for ", folder)
+    const propertiesToCompare = extractProperties(contentToCompare);
+    console.log("target properties extracted for ", folder);
 
-        if (!contentToCompare) {
-            console.error(`Failed to fetch ${folder}/environment.prod.ts`);
-            continue;
-        }
+    console.log('-----------------------');
 
-        console.log("extracting base properties...")
-        const baseProperties = extractProperties(baseContent);
-        console.log("base properties extracted...")
+    console.log(`Missing properties in ${folder}:`);
+    const basePropertiesSet = new Set(baseProperties)
+    const propertiesToCompareSet = new Set(propertiesToCompare)
 
+    const missingProperties = [];
+    basePropertiesSet.forEach(item => {
+      if (!propertiesToCompareSet.has(item)) {
+        missingProperties.push(item);
+      }
+    });
+    // const missingProperties = baseProperties.filter(prop => !propertiesToCompare.includes(prop));
+    missingProperties.forEach(prop => console.log(prop));
 
-        console.log("extracting target properties...")
-        const propertiesToCompare = extractProperties(contentToCompare);
-        console.log("target properties extracted...")
-
-        console.log('-----------------------');
-
-        console.log(`Missing properties in ${folder}:`);
-        const basePropertiesSet = new Set(baseProperties)
-        const propertiesToCompareSet = new Set(propertiesToCompare)
-
-        const missingProperties = [];
-        basePropertiesSet.forEach(item => {
-            if (!propertiesToCompareSet.has(item)) {
-                missingProperties.push(item);
-            }
-        });
-        // const missingProperties = baseProperties.filter(prop => !propertiesToCompare.includes(prop));
-        missingProperties.forEach(prop => console.log(prop));
-
-        if (missingProperties.length > 0) {
-            await addMissingPropertiesToFile(folder, contentToCompare, missingProperties);
-            res.status(200).json({ message: 'Missing properties added and changes committed.' });
-        } else {
-            console.log(`No missing properties in ${folder}`);
-            res.status(200).json({ message: 'No missing properties found.' });
-        }
-
-        console.log('-----------------------');
+    if (missingProperties.length > 0) {
+      // await addMissingPropertiesToFile(folder, contentToCompare, missingProperties);
+      // res.status(200).json({ message: 'Missing properties added and changes committed.' });
+      // res.status(200).json({ message: 'Missing properties are found' });
+    } else {
+      console.log(`No missing properties in ${folder}`);
+      // res.status(200).json({ message: 'No missing properties found.' });
     }
 
-    // const missingProperties = getMissingProperties(folder);
+    console.log('-----------------------');
+    console.log('-----------------------');
+  }
 
-    // if (missingProperties.length > 0) {
-    //     await addMissingProperties(folder, missingProperties);
-    //     await commitChanges(folder);
-    //     res.status(200).json({ message: 'Missing properties added and changes committed.' });
-    // } else {
-    //     res.status(200).json({ message: 'No missing properties found.' });
-    // }
+  res.send('Comparison Done!');
+
+
+  // const missingProperties = getMissingProperties(folder);
+
+  // if (missingProperties.length > 0) {
+  //     await addMissingProperties(folder, missingProperties);
+  //     await commitChanges(folder);
+  //     res.status(200).json({ message: 'Missing properties added and changes committed.' });
+  // } else {
+  //     res.status(200).json({ message: 'No missing properties found.' });
+  // }
 });
 
 async function fetchEnvironmentFileContent(filePath) {
-    const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
-    try {
-        const response = await fetch(apiUrl, {
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        });
+  const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`;
+  try {
+    const response = await fetch(apiUrl, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-        if (response.ok) {
-            const data = await response.json();
-            const fileContentResponse = await fetch(data.download_url);
-            const fileContent = await fileContentResponse.text();
-            return fileContent;
-        } else {
-            throw new Error(`Failed to fetch ${filePath}`);
-        }
-    } catch (error) {
-        console.error(error);
-        return null;
+    if (response.ok) {
+      const data = await response.json();
+      const fileContentResponse = await fetch(data.download_url);
+      const fileContent = await fileContentResponse.text();
+      return fileContent;
+    } else {
+      throw new Error(`Failed to fetch ${filePath}`);
     }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
 function extractProperties(fileContent) {
-    // Extract property keys using a regular expression
-    const propertyPattern = /^[ \t]*([a-zA-Z_][a-zA-Z0-9_]*)[ \t]*:[ \t]*(.*)/gm;
-    const matches = fileContent.match(propertyPattern);
-    if (matches) {
-      return matches.map(match => match.trim().split(':')[0].trim());
-    }
-    console.log("match:",matches)
-    return matches || [];
+  // Extract property keys using a regular expression
+  const propertyPattern = /^[ \t]*([a-zA-Z_][a-zA-Z0-9_]*)[ \t]*:[ \t]*(.*)/gm;
+  const matches = fileContent.match(propertyPattern);
+  if (matches) {
+    return matches.map(match => match.trim().split(':')[0].trim());
+  }
+  console.log("match:", matches)
+  return matches || [];
+}
+
+async function addMissingPropertiesToFile(folder, contentOnTargetFolder, missingProperties) {
+  // const lines = contentOnTargetFolder.split('\n');
+
+  // // console.log("Lines", lines);
+  // // Add the missing properties to the content
+  // const updatedContent = lines.map(line => {
+  //   if (line.includes('export const environment = {')) {
+  //     return [...line, ...missingProperties].join(',\n');
+  //   }
+  //   return line;
+  // }).join('\n');
+
+  const insertionPoint = contentOnTargetFolder.lastIndexOf('}');
+  if (insertionPoint !== -1) {
+    const propertiesToAdd = missingProperties.map(prop => `  ${prop}: 'NEW_VALUE',`).join('\n');
+    contentOnTargetFolder = contentOnTargetFolder.slice(0, insertionPoint) + propertiesToAdd + contentOnTargetFolder.slice(insertionPoint);
   }
 
-  async function addMissingPropertiesToFile(folder, contentOnTargetFolder, missingProperties){
-    // const lines = contentOnTargetFolder.split('\n');
-  
-    // // console.log("Lines", lines);
-    // // Add the missing properties to the content
-    // const updatedContent = lines.map(line => {
-    //   if (line.includes('export const environment = {')) {
-    //     return [...line, ...missingProperties].join(',\n');
-    //   }
-    //   return line;
-    // }).join('\n');
-  
-    const insertionPoint = contentOnTargetFolder.lastIndexOf('}');
-      if (insertionPoint !== -1) {
-        const propertiesToAdd = missingProperties.map(prop => `  ${prop}: 'NEW_VALUE',`).join('\n');
-        contentOnTargetFolder = contentOnTargetFolder.slice(0, insertionPoint) + propertiesToAdd + contentOnTargetFolder.slice(insertionPoint);
-      }
-  
-    // console.log('Added missing properties: ', contentOnTargetFolder);
-  
-    await commitChanges(folder, contentOnTargetFolder);
-  }
-  
-  async function commitChanges(folder, content) {
-    const octokit = new Octokit({
-      auth: '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' // Replace with your GitHub Personal Access Token
+  // console.log('Added missing properties: ', contentOnTargetFolder);
+
+  await commitChanges(folder, contentOnTargetFolder);
+}
+
+async function commitChanges(folder, content) {
+  const octokit = new Octokit({
+    auth: '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' // Replace with your GitHub Personal Access Token
+  });
+
+  const commitMessage = 'Added Missing Properties';
+
+  try {
+
+    // Fetch the current SHA-1 hash of the file
+    const fileInfo = await octokit.rest.repos.getContent({
+      owner: 'QQyrus', // Replace with your GitHub username
+      repo: 'ui-configuration',
+      path: `${folder}/environment.prod.ts`
     });
-  
-    const commitMessage = 'Added Missing Properties';
-  
-    try {
-  
-      // Fetch the current SHA-1 hash of the file
-      const fileInfo = await octokit.rest.repos.getContent({
-        owner: 'QQyrus', // Replace with your GitHub username
-        repo: 'ui-configuration',
-        path: `${folder}/environment.prod.ts`
-      });
-  
-      const response = await octokit.rest.repos.createOrUpdateFileContents({
-        owner: 'QQyrus', // Replace with your GitHub username
-        repo: 'ui-configuration',
-        path: `${folder}/environment.prod.ts`,
-        message: commitMessage,
-        content: Buffer.from(content).toString('base64'),
-        sha: fileInfo.data.sha // Provide the current SHA-1 hash of the file
-      });
-  
-      console.log(`Changes committed to ${folder}/environment.prod.ts: ${response.data.commit.html_url}`);
-    } catch (error) {
-      console.error(`Failed to commit changes to ${folder}/environment.prod.ts`, error);
-    }
+
+    const response = await octokit.rest.repos.createOrUpdateFileContents({
+      owner: 'QQyrus', // Replace with your GitHub username
+      repo: 'ui-configuration',
+      path: `${folder}/environment.prod.ts`,
+      message: commitMessage,
+      content: Buffer.from(content).toString('base64'),
+      sha: fileInfo.data.sha // Provide the current SHA-1 hash of the file
+    });
+
+    console.log(`Changes committed to ${folder}/environment.prod.ts: ${response.data.commit.html_url}`);
+  } catch (error) {
+    console.error(`Failed to commit changes to ${folder}/environment.prod.ts`, error);
   }
+}
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
